69089cfbd2214981da819119b89a441af477abbd
Matteo Bonamassa <matteo.bonamassa1@gmail.com>
2020-03-23 12:39:16 +0100

    Add part 2 & 3 of ex1.1

--- NSL-01/LSN_Exercise_011.cpp
+++ NSL-01/LSN_Exercise_011.cpp
@@ -33,28 +33,71 @@ int main(int argc, char *argv[]){
 	} else cerr << "PROBLEM: Unable to open seed.in" << endl;
 
 
-	int M = 100000;			// Total number of throws
+	int M = 1000000;			// Total number of throws
 	int N = 100;                 	// Number of blocks
 	int L = static_cast<int>(M/N);  // Number of throws in each block, please use for M a multiple of N
 	double *random_vec = new double[M]();		// Define random vector
-	double *average = new double[N]();		// Define average vector
-	double *average_sqr = new double[N]();		// Define average squared vector
+	double *average1 = new double[N]();		// Define average vector
+	double *average_sqr1 = new double[N]();		// Define average squared vector
 
 	for(int i=0; i < M; i++){	// Load the vector with random number distributed uniformly
 		random_vec[i] = rnd.Rannyu();
 	}
 
+	// Estimate of <r>
 	for(int i=0; i < N; i++){       // Compute the average of my observable and the aveˆ2 to calculate the variance
 		double sum = 0;
 		for(int j=0; j < L; j++){
 			int k = j + i*L;
 			sum = sum + random_vec[k];
 		}
-		average[i] = sum/L;
-		average_sqr[i] = pow(average[i],2);
+		average1[i] = sum/L;
+		average_sqr1[i] = pow(average1[i],2);
 	}
 	
-	prog_average_std_dev_block_method("data/EX011.dat", average, average_sqr, N);	
+	prog_average_std_dev_block_method("data/EX011(1).dat", average1, average_sqr1, N);	
+
+	// Estimate of <(sigma_r)^2>
+        double *average2 = new double[N]();             // Define average vector
+        double *average_sqr2 = new double[N]();         // Define average squared vector
+
+	for(int i=0; i < N; i++){       // Compute the average of my observable and the aveˆ2 to calculate the variance
+                 double sum = 0;
+                 for(int j=0; j < L; j++){
+                         int k = j + i*L;
+                         sum = sum + pow((random_vec[k]-0.5),2);
+                 }
+                 average2[i] = sum/L;
+                 average_sqr2[i] = pow(average2[i],2);
+         }
+ 
+         prog_average_std_dev_block_method("data/EX011(2).dat", average2, average_sqr2, N);
+
+	// Estimate chi^2 dividing [0,1] in M=10^2 part
+	int intervals = 100;
+	double interval_prob = 1./intervals;		// Probability to sit in a specific sub-interval due to uniform distr
+	int throws = static_cast<int>(M/intervals); 	// Throws in each sub-interval	
+	double expected_value = throws/intervals;	// Expected value due to uniform distribution
+	double *observations = new double[intervals]();
+
+	for(int i=0; i < intervals; i++){
+		int n_hits = 0;
+		for(int j=0; j < throws ;j++){
+			int k = j + i*throws;
+			if( random_vec[k] >= (0 + i*interval_prob) && random_vec[k] <= (i*interval_prob + interval_prob)){
+				n_hits += 1;
+			}
+		observations[i] = n_hits;
+		}	
+	};
+
+	double *expected_value_vec = new double[intervals]();
+	for(int i=0; i< intervals; i++) expected_value_vec[i] = expected_value;
+
+	ofstream out_file;
+        out_file.open("data/EX011(3).dat");
+	out_file << chi_sqrd(observations, expected_value_vec, expected_value_vec, intervals);
+	out_file.close();
 
 	return 0;
 }
