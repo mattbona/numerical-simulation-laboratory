08ad4e5938e6e389e6e771f7fef70596a8b503ba
Matteo Bonamassa <matteo.bonamassa1@gmail.com>
2020-03-28 16:42:26 +0100

    Complete ex 2.2.1

--- NSL-02/LSN_Exercise_02_2.cpp
+++ NSL-02/LSN_Exercise_02_2.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-double sqr_euclidean_distance(double* pointA, double* pointB);
+double euclidean_distance(double* pointA, double* pointB);
 
 int main(int argc, char *argv[]){
 
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]){
 	} else cerr << "PROBLEM: Unable to open seed.in" << endl;
 
 
-	int M = 1E4;	        		// Total number of throws
+	int M = 1E6;	        		// Total number of throws
 	int N = 1E2;                 	// Number of steps
 	int L = static_cast<int>(M/N);  // Number of repetition for the trajectory, please use for M a multiple of N
 	double *random_vec = new double[M]();		// Define random vector
@@ -49,8 +49,8 @@ int main(int argc, char *argv[]){
     }
 
 // Estimate of the 3D distance on a cubic lattice done by a RW
-	double *average_sqr_RW_distance_vec1 = new double[N]();		    // Define average vector
-	double *sqr_average_sqr_RW_distance_vec1 = new double[N]();		// Define average squared vector
+	double *sum_RW_distance_vec1 = new double[N]();		    // Define average vector
+	double *sum_sqr_RW_distance_vec1 = new double[N]();		// Define average squared vector
 	double *origin = new double[3];
     origin[0] = 0;
     origin[1] = 0;
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]){
 
 		for(int j=0; j < N; j++){                  // Cycle over the steps of one trajectory
 			int k = j + i*N;
-		    double sqr_RW_distance = 0;
+		    double RW_distance = 0;
 
             if(random_vec[k]> 0 && random_vec[k]<= 1/3.){               // Select the x-direction
                 if(coin[k] <= prob_backw){ walker_head[0] += -lattice_constant; }
@@ -79,26 +79,25 @@ int main(int argc, char *argv[]){
                 else { walker_head[2] += lattice_constant; }
             }
 		        
-            sqr_RW_distance = sqr_euclidean_distance(origin, walker_head);
-		    average_sqr_RW_distance_vec1[j] += sqr_RW_distance;
-		    sqr_average_sqr_RW_distance_vec1[j] += pow(average_sqr_RW_distance_vec1[j],2.);
+            RW_distance = euclidean_distance(origin, walker_head);
+		    sum_RW_distance_vec1[j] += RW_distance;
+		    sum_sqr_RW_distance_vec1[j] += pow(RW_distance,2.);
         }
     
-    cout << average_sqr_RW_distance_vec1[0]/L << "\t" << sqr_average_sqr_RW_distance_vec1[0]/L << endl;
     } 
-
-
+    
+    ofstream out_file;
+    out_file.open("data/EX02_2(1).dat");
     for(int j=0; j < N; j++){ 
-//       cout << average_sqr_RW_distance_vec1[j] << "\t" << sqr_average_sqr_RW_distance_vec1[j] << endl;
-//       cout << sqrt(average_sqr_RW_distance_vec1[i])<< "\t" << sqrt(std_dev(average_sqr_RW_distance_vec1[i], sqr_average_sqr_RW_distance_vec1[i], L)) << endl; 
-        
+       out_file << sum_RW_distance_vec1[j]/L << " " << std_dev(sum_RW_distance_vec1[j]/L,sum_sqr_RW_distance_vec1[j]/L,L) << endl;
    }
-
+    out_file.close();
+    
     return 0;
 }
 
-double sqr_euclidean_distance(double* pointA, double* pointB){
+double euclidean_distance(double* pointA, double* pointB){
 
-    return pow(pointA[0]-pointB[0],2.) + pow(pointA[1]-pointB[1],2.) + pow(pointA[2]-pointB[2],2.);
+    return sqrt( pow(pointA[0]-pointB[0],2.) + pow(pointA[1]-pointB[1],2.) + pow(pointA[2]-pointB[2],2.) );
 
 };
