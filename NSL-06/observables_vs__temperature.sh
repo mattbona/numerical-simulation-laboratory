0acc8ab9b27f092f8c981c3658a7ac73647532b8
Matteo Bonamassa <matteo.bonamassa1@gmail.com>
2020-05-24 17:38:19 +0200

    Add script for temp

---
+++ NSL-06/observables_vs__temperature.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+while read temp; do
+    echo "### Computing $temp T"
+    sed -i "1s/TEMPERATURE/$temp/" input.dat
+    ./Monte_Carlo_ISING_1D.exe
+    sed -i "1s/$temp/TEMPERATURE/" input.dat
+done <temperatures.dat
